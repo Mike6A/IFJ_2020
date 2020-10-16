cbafb2b49a28df801797dc08d5cc65eec73b85bb
Michal Mlčoch <mike.dormio@gmail.com>
2020-10-16 15:45:25 +0200

    add enum for EOLflag

--- scenner.h
+++ scenner.h
@@ -3,6 +3,7 @@
 #include "stdbool.h"
 
 typedef enum { KW, INT, DOUBLE, STRING, NONE } TokenType;
+typedef enum { EOL_REQUIRED, EOL_OPTIONAL, EOL_FORBIDEN } EOLflag;
 
 char RUNE_LITERALS[] = {'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', '"', '\''};
 
