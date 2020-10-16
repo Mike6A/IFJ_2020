378eb69e7605aa277e751b34706f12deb38bf320
Michal Mlčoch <mike.dormio@gmail.com>
2020-10-16 23:29:38 +0200

    add func declaration

--- scanner.h
+++ scanner.h
@@ -8,8 +8,22 @@ typedef enum { EOL_REQUIRED, EOL_OPTIONAL, EOL_FORBIDEN } EOLflag;
 
 char RUNE_LITERALS[] = {'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', '"', '\''};
 char SEPARATORS[] = {' ', '\t'};
+char* KEYWORDS[] = {"if", "else", "float64", "for", "func", "int", "package", "return", "string"};
 
 typedef struct {
     TokenType type;
     char* value;
 } tToken;
+
+void getNextChar();
+bool isActLetter();
+bool isActNumber();
+bool is(char arr[]);
+
+bool appendChar();
+void clearString();
+char* copyString();
+
+tToken getToken(EOLflag eolFlag, int* errCode);
+tToken state_EOLRequired(EOLflag eolFlag, int* errCode);
+tToken state_ID(EOLflag eolFlag, int* errCode);
