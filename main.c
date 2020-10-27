ef381d6118fdb0e6787fb4a4ee3b3953e61b91c8
qwerlkj <qwerlkj@azet.sk>
2020-10-27 21:26:16 +0100

    Added testing syntaxTree creation.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "scanner.h"
 #include "symtable/symtable.h"
+#include "syntaxTree.h"
 
 //eg.
 extern char* KEYWORDS[];
@@ -7,13 +8,13 @@ extern char* KEYWORDS[];
 char* getEnumString(TokenType type){
     switch (type)
     {
-        case t_ID: return "ID";
-        case t_KW: return "KW";
-        case t_INT: return "INT";
-        case t_DOUBLE: return "DOUBLE";
-        case t_STRING: return "STRING";
-        case t_NONE: return "NONE";
-        case t_EOF: return "EOF";
+        case tokenType_ID: return "ID";
+        case tokenType_KW: return "KW";
+        case tokenType_INT: return "INT";
+        case tokenType_DOUBLE: return "DOUBLE";
+        case tokenType_STRING: return "STRING";
+        case tokenType_NONE: return "NONE";
+        case tokenType_EOF: return "EOF";
         default: return "";
     }
 }
@@ -23,15 +24,15 @@ void test_tokenizer(){
     initTokenizer(&tokenizer);
     do {
         getToken(&tokenizer); 
-        printf("%20s | %6s | err: %d\n", tokenizer.outputToken.value, getEnumString(tokenizer.outputToken.type), tokenizer.errorCode); 
-        if (tokenizer.outputToken.type != t_EOF && tokenizer.errorCode == 0) {
-            if (tokenizer.outputToken.type != t_NONE){
+        //printf("%20s | %6s | err: %d\n", tokenizer.outputToken.value, getEnumString(tokenizer.outputToken.type), tokenizer.errorCode);
+        /*if (tokenizer.outputToken.type != tokenType_EOF && tokenizer.errorCode == 0) {
+            if (tokenizer.outputToken.type != tokenType_NONE){
                 free(tokenizer.outputToken.value);
             } else if (strlen(tokenizer.outputToken.value) > 1) {
                 free(tokenizer.outputToken.value);
             }
-        }
-    } while (tokenizer.outputToken.type != t_EOF);
+        }*/
+    } while (tokenizer.outputToken.type != tokenType_EOF);
     destructBuilder(&tokenizer.sb);
 }
 
@@ -51,7 +52,31 @@ void test_hashtable(){
     destructHashTable(&table);
 }
 
+void test_tree(){
+    tTokenizer tokenizer;
+    initTokenizer(&tokenizer);
+
+    getToken(&tokenizer);
+    SyntaxNode* exp = ParseExpression(&tokenizer);
+    printSyntaxTree(exp, "", true);
+    deleteSyntaxTree(exp);
+//    printSyntaxTree(exp, "", true);
+    destructBuilder(&tokenizer.sb);
+
+}
+
+void test_GetTOKEN() {
+    tTokenizer tokenizer;
+    initTokenizer(&tokenizer);
+    do {
+        getToken(&tokenizer);
+        //printf("%s\n", tokenizer.outputToken.value);
+    } while (tokenizer.outputToken.type != tokenType_EOF);
+
+    destructBuilder(&tokenizer.sb);
+}
+
 int main() {
-    test_hashtable();
+    test_tree();
 	return 1;
 }
