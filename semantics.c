6bfe323fb1c04e8ba1de31fa46440c69b60b5967
xburan03 <xburan03@stud.fit.vutbr.cz>
2020-11-11 19:11:24 +0100

    Initial semantics commit.

---
+++ semantics.c
@@ -0,0 +1,6 @@
+/**
+ * @file semantics.c
+ * @author Radim Buráň (xburan03@stud.fit.vutbr.cz)
+ * @brief Implementation of semantics for IFJ20.
+ */
+
---
+++ semantics.h
@@ -0,0 +1,10 @@
+/**
+ * @file semantics.h
+ * @author Radim Buráň (xburan03@stud.fit.vutbr.cz)
+ * @brief Header file for semantics.
+ */
+
+#ifndef IFJ_2020_SEMANTICS_H
+#define IFJ_2020_SEMANTICS_H
+
+#endif //IFJ_2020_SEMANTICS_H
