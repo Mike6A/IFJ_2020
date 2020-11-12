b8ed5d4a992785f84b21e67fc34bf767ab617150
Michal Mlčoch <mike.dormio@gmail.com>
2020-11-12 14:55:25 +0100

    more beuty test bash

--- syntaxTree-test/VGcheck.sh
+++ syntaxTree-test/VGcheck.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+count_OK_mem=0
+count_ERR_mem=0
+count_OK_err=0
+count_ERR_err=0
 for i in ./*
 do
 	if [[ $i == *"linux"* ]] || [[ $i == *"win"* ]]; then
@@ -6,15 +10,24 @@ do
 		RESULT_MEM=$(grep "All heap blocks were freed" <<< "$RESULT" | wc -l)
 		RESULT_ERR=$(grep "ERROR SUMMARY: 0" <<< "$RESULT" | wc -l)
 		if [ "$RESULT_MEM" -eq "0" ] ; then
-			echo $i : "MEMORY LEAK"
+			echo -e "$i : \e[31mMEMORY LEAK\e[0m"
+			count_ERR_mem=$((count_ERR_mem+1))
 		else
-			echo $i : "MEM: OK"
+			echo -e "$i : \e[32mMEM: OK\e[0m"
+			count_OK_mem=$((count_OK_mem+1))
 		fi
 		if [ "$RESULT_ERR" -eq "0" ] ; then
-			echo $i : "ERROR from context"
+			echo -e "$i : \e[31mERROR from context\e[0m"
+			count_ERR_err=$((count_ERR_err+1))
 		else
-			echo $i : "ERR: OK"
+			echo -e "$i : \e[32mERR: OK\e[0m"
+			count_OK_err=$((count_OK_err+1))
 		fi
 		echo "-----------------------------------------------"
 	fi
 done
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
+echo -e "MEM:{\e[32mOK: $count_OK_mem\e[0m ,\e[31mERR: $count_ERR_mem\e[0m} | CON:{\e[32mOK: $count_OK_err\e[0m ,\e[31mERR: $count_ERR_err\e[0m}"
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
