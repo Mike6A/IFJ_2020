08faf2583e82595f7890be882ca9e7debca30d6a
Hudzino <michal.mikota@gmail.com>
2020-11-19 21:17:02 +0100

    rewrite macros to functions and tested

--- gen_code.c
+++ gen_code.c
@@ -9,90 +9,102 @@
 #include <stdio.h>
 
 /// built-in string functions
-#define BIF_LENGTH 
-do{\
-    "# func len(s string) (int)\n"
-    "LABEL _func_len\n"
-    "PUSHFRAME\n"
-    "DEFVAR LF@str_len\n"
-    "STRLEN LF@str_len LF@arg_0\n"
-    "POPFRAME\n"
-    "RETURN\n"
-}while(0)
 
-#define BIF_ORD
-do{\
-    "# func ord(s string,i int) (int, int)\n");
-    "LABEL _func_ord"
-    "PUSHFRAME"
-    "DEFVAR LF@final_int"
-    "DEFVAR LF@in_string"
-    "MOVE LF@in_string bool@true"
-    "LT LF@in_string LF@arg_1 int@0"
-    "JUMPIFEQ _func_error LF@in_string bool@true"
-    "DEFVAR LF@max_int_value"
-    "STRLEN LF@max_int_value LF@arg_0"
-    "SUB LF@max_int_value LF@max_int_value int@1"
-    "GT LF@in_string LF@arg_1 LF@max_int_value" 
-    "JUMPIFEQ _func_error LF@in_string bool@true" 
-    "STRI2INT LF@final_int LF@arg_0 LF@arg_1" 
-    "LABEL _func_error"
-    "POPFRAME"
-    "RETURN"
-}while(0)
+void bif_lenght()
+{
 
-#define BIF_CHR
-do{\
+    printf("# func len(s string) (int)\n");
+    printf("LABEL _func_len\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@str_len\n");
+    printf("STRLEN LF@str_len LF@arg_0\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 
-    "# func chr(i int) (string, int)\n"
-    "LABEL _func_chr\n"
-    "PUSHFRAME\n"
-    "DEFVAR LF@final_char\n"
-    "MOVE LF@final_char string@\n"
-    "DEFVAR LF@in_string\n"
-    "MOVE LF@in_string bool@true\n"
-    "LT LF@in_string LF@arg_0 int@0\n"
-    "JUMPIFEQ _func_error LF@in_string bool@true\n"
-    "GT LF@in_string LF@arg_0 int@255\n" 
-    "JUMPIFEQ _func_error LF@in_string bool@true\n" 
-    "INT2CHAR LF@final_char LF@arg_0\n" 
-    "LABEL _func_error\n"
-    "POPFRAME\n"
-    "RETURN\n"
-}while(0)
+}
+
+void bif_ord()
+{
+
+    printf("# func ord(s string,i int) (int, int)\n");
+    printf("LABEL _func_ord\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@final_int\n");
+    printf("DEFVAR LF@in_string\n");
+    printf("MOVE LF@in_string bool@true\n");
+    printf("LT LF@in_string LF@arg_1 int@0\n");
+    printf("JUMPIFEQ _func_error LF@in_string bool@true\n");
+    printf("DEFVAR LF@max_int_value\n");
+    printf("STRLEN LF@max_int_value LF@arg_0\n");
+    printf("SUB LF@max_int_value LF@max_int_value int@1\n");
+    printf("GT LF@in_string LF@arg_1 LF@max_int_value\n");
+    printf("JUMPIFEQ _func_error LF@in_string bool@true\n" );
+    printf("STRI2INT LF@final_int LF@arg_0 LF@arg_1\n" );
+    printf("LABEL _func_error\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+
+}
+
+
+void bif_chr()
+{
+
+    printf("# func chr(i int) (string, int)\n");
+    printf("LABEL _func_chr\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@final_char\n");
+    printf("MOVE LF@final_char string@\n");
+    printf("DEFVAR LF@in_string\n");
+    printf("MOVE LF@in_string bool@true\n");
+    printf("LT LF@in_string LF@arg_0 int@0\n");
+    printf("JUMPIFEQ _func_error LF@in_string bool@true\n");
+    printf("GT LF@in_string LF@arg_0 int@255\n");
+    printf("JUMPIFEQ _func_error LF@in_string bool@true\n");
+    printf("INT2CHAR LF@final_char LF@arg_0\n");
+    printf("LABEL _func_error\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+
+}
 
 ///build-in functions for data type conversion
 
-#define BIF_INT2FLOAT
-do{\
+void bif_int2float()
+{
 
-    "#func int2float(i int) (float64)\n"
-    "LABEL _func_int2float\n"
-    "PUSHFRAME\n"
-    "DEFVAR LF@final_float\n"
-    "INT2FLOAT LF@final_float LF@arg_0\n"
-    "POPFRAME\n"
-    "RETURN\n"
+    printf("# func int2float(i int) (float64)\n");
+    printf("LABEL _func_int2float\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@final_float\n");
+    printf("INT2FLOAT LF@final_float LF@arg_0\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 
-}while(0);
+}
 
-#define BIF_FLOAT2INT
-do{\
+void bif_float2int()
+{
 
-    "#func float2int(f float64) (int)\n"
-    "LABEL _func_float2int\n"
-    "PUSHFRAME\n"
-    "DEFVAR LF@final_int\n"
-    "int2float LF@final_int LF@arg_0\n"
-    "POPFRAME\n"
-    "RETURN\n"
+    printf("# func float2int(f float64) (int)\n");
+    printf("LABEL _func_float2int\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@final_int\n");
+    printf("FLOAT2INT LF@final_int LF@arg_0\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 
-}while(0);
-
-
+}
 
 int main()
 {
+
+    bif_lenght();
+    bif_ord();
+    bif_chr();
+    bif_int2float();
+    bif_float2int();
+
 
     return 0;
 }
