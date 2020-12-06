93f0b3866cb1ff962ca761a8f0a7f51bce52b251
qwerlkj <qwerlkj@azet.sk>
2020-12-06 23:47:15 +0100

    Umplemented stack for IF and FOR

--- stack.h
+++ stack.h
@@ -34,4 +34,18 @@ void createList(tStringLinkedListItem* list);
 int addListItem(tStringLinkedListItem* list, char* str);
 int destroyList(tStringLinkedListItem* list);
 
+#define MAX_STACK 100//DANGER!!!
+
+typedef struct t_INT_Stack {
+    int data[MAX_STACK];
+    int top;
+} t_INT_Stack;
+
+void init_INT_Stack(t_INT_Stack* stack);
+int add_INT_StackItem(t_INT_Stack* stack, int data);
+int destroy_INT_Stack(t_INT_Stack* stack);
+bool is_INT_Stack_Empty(t_INT_Stack* stack);
+int top_INT_Stack(t_INT_Stack* stack);
+int pop_INT_Stack(t_INT_Stack* stack);
+
 #endif //IFJ_2020_STACK_H
